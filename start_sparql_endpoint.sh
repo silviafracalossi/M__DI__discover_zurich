86995e9fe1f8b671408e52bfbcc255a6db845df6
Fracalossi Silvia <silvia.fracalossi@stud-inf.unibz.it>
2020-06-11 22:41:37 +0200

    Set up the sparql endpoint and the bush script for project

---
+++ start_sparql_endpoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash /mnt/d/Programmi/ontop-cli-4.0.0-beta-2-SNAPSHOT-geof/ontop endpoint --ontology=applications_files/discover_zurich-protege_ontology.owl --mapping=applications_files/discover_zurich-protege_ontology.obda --properties=applications_files/discover_zurich-protege_ontology.properties --cors-allowed-origins=http://yasgui.org
