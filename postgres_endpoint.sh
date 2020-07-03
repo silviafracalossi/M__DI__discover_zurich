9e0d4dc7edd59b014dcbd479ebb3aaed4f4dae17
Fracalossi Silvia <silvia.fracalossi@stud-inf.unibz.it>
2020-07-03 16:28:15 +0200

    Changed configuration to ontop endpoint with postgres

---
+++ postgres_endpoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash /mnt/d/Programmi/ontop-cli-4.0.0-beta-2-SNAPSHOT-geof-updated/ontop endpoint --ontology=applications_files/discover_zurich-protege_ontology_postgres.owl --mapping=applications_files/discover_zurich-protege_ontology_postgres.obda --properties=applications_files/discover_zurich-protege_ontology_postgres.properties --cors-allowed-origins=http://yasgui.org
